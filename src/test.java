9a9a32a9cf1028d14e45745a9739665f58c97dbd
Kephael <me@caseybaer.com>
2015-10-12 20:37:32 -0400

    Added Twitter 4J 4.04 to project and removed non functional demonstration code.

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterAPIConfiguration;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.api.TweetsResources;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
@@ -13,23 +16,8 @@ import java.util.Properties;
 
 public class test {
 
-	public static void main(String[] args) {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
+	public static void main(String[] args) throws TwitterException {
 		
-		// Setting Tokens
-		cb.setDebugEnabled(true)
-			.setOAuthConsumerKey("b8clPMAjQs4ROlWtctk3jarYl")
-			.setOAuthConsumerSecret("Ngf0aWbLNz2j0VOxKQWVI0t6XoqN2Xd23lGDKXGKF6dk7c6vka")
-			.setOAuthAccessToken("3111937349-G8iXjHIvSFi73oLFyEL3SPeAZ0V4nzatBa0sWWe")
-			.setOAuthAccessTokenSecret("gv8JtAyfovxftZ423mBkXcG1IgrbeqiXV7MhGartzKYE2");
-		
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		twitter4j.TwitterAPIConfiguration tw = tf.getInstance();
-		
-		// Test
-		// Post on twitter time line
-		Status stat = tw.updateStatus("Welcome to Twiiter");
-		System.out.println("Twitter updated");
 	}
 
 }
