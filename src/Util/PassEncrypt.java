9c2c0d83d3a253007f26009f68efd4f262cc5baf
linuxguy467 <dpplrdr435@gmail.com>
2015-12-05 19:39:41 -0500

    Update PassEncrypt.java
    Added methods to read and write API settings

--- src/Util/PassEncrypt.java
+++ src/Util/PassEncrypt.java
@@ -1,15 +1,24 @@
 package Util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+
 import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+
+import java.util.Properties;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
+
+import twitter4j.conf.Configuration;
 
 /**
  * Two-Way Encryption Algorithm
@@ -20,7 +29,7 @@ import javax.crypto.spec.PBEParameterSpec;
 public class PassEncrypt {
 
 	/**Master Password for PBE */
-	private static final char[] PASSWORD = "Miester9000@30Hz".toCharArray();
+	private static final char[] PASSKEY = "Miester9000@30Hz".toCharArray();
 	
 	/**Salt byte array for encoding and decoding*/
 	private static byte[] salt = new byte[8];
@@ -28,43 +37,135 @@ public class PassEncrypt {
 	/**Generates random byte values*/
 	private static SecureRandom secureRandGen;
 	
-	public PassEncrypt() throws NoSuchAlgorithmException
-	{
-		secureRandGen = SecureRandom.getInstance("SHA1PRNG");
-		secureRandGen.nextBytes(salt);
-	}
+	/*Output stream*/
+	private static FileOutputStream output;
+	
+	/*Uses text file for testing purposes*/
+	private static File file = new File("twitter4j.properties");
+	
 	
 	/**
-	 * Encrypts string
-	 * @param property the string
+	 * Encrypts configuration property string
+	 * @param configuration config property 
 	 * @return encrypted string
-	 * @throws GeneralSecurityException
-	 * @throws UnsupportedEncodingException
 	 */
-	public static String encrypt(String property) throws GeneralSecurityException, UnsupportedEncodingException
+	private static String encrypt(String configuration) 
 	{
-		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-		SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
-		Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDes");
-		pbeCipher.init(Cipher.ENCRYPT_MODE, key, new PBEParameterSpec(salt, 20));
-		String encrypted = new String(pbeCipher.doFinal(property.getBytes("UTF-8")));
+		String encrypted = "";
+		try {
+			secureRandGen = SecureRandom.getInstance("SHA1PRNG");
+			secureRandGen.nextBytes(salt);
+			SecretKeyFactory keyHolder = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+			SecretKey desPBEkey = keyHolder.generateSecret(new PBEKeySpec(PASSKEY));
+			Cipher cipher = Cipher.getInstance("PBEWithMD5AndDes");
+			cipher.init(Cipher.ENCRYPT_MODE, desPBEkey, new PBEParameterSpec(salt, 20));
+			encrypted = new String(cipher.doFinal(configuration.getBytes("UTF-8")));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (GeneralSecurityException ge)
+		{
+			ge.printStackTrace();
+		}
 		return encrypted;
 	}
 	
 	/**
 	 * Decrypts the encrypted string
-	 * @param property encrypted string
+	 * @param encrypted encrypted string
 	 * @return decrypted string
-	 * @throws GeneralSecurityException
-	 * @throws UnsupportedEncodingException
 	 */
-	public static String decrypt(String property) throws GeneralSecurityException, UnsupportedEncodingException
+	public static String decrypt(String encrypted)
 	{
-		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-		SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
-		Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDes");
-		pbeCipher.init(Cipher.DECRYPT_MODE, key, new PBEParameterSpec(salt, 20));
-		String decrypted = new String(pbeCipher.doFinal(property.getBytes("UTF-8")), "UTF-8");
-		return decrypted;
+		String decrypted = "";
+		try{
+			secureRandGen = SecureRandom.getInstance("SHA1PRNG");
+			secureRandGen.nextBytes(salt);
+			SecretKeyFactory keyHolder = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+			SecretKey desKey = keyHolder.generateSecret(new PBEKeySpec(PASSKEY));
+			Cipher cipher = Cipher.getInstance("PBEWithMD5AndDes");
+			cipher.init(Cipher.DECRYPT_MODE, desKey, new PBEParameterSpec(salt, 20));
+			decrypted = new String(cipher.doFinal(encrypted.getBytes()));
+		}catch(GeneralSecurityException ge)
+		{
+			ge.printStackTrace();
+		}
+			return decrypted;
+	}
+	
+	/**
+	 * creates a file with encrypted settings
+	 * @param Configuration config
+	 */
+	public static void writeEncryptedSettings(Configuration config)
+	{
+		try {
+			file.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		Properties configProperties = new Properties();
+		
+		configProperties.setProperty("oauth.consumerKey:", 
+					encrypt(config.getOAuthConsumerKey()));
+		configProperties.setProperty("oauth.consumerSecret:", 
+					encrypt(config.getOAuthConsumerSecret()));
+		configProperties.setProperty("oauth.accessToken:", 
+					encrypt(config.getOAuthAccessToken()));
+		configProperties.setProperty("oauth.accessTokenSecret:", 
+					encrypt(config.getOAuthAccessTokenSecret()));
+		
+		try 
+		{
+			output = new FileOutputStream(file);
+			configProperties.store(output, "Twitter4j Properties");
+		} catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+			
+		} catch(IOException ie)
+		{
+			ie.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Reads file to get the API settings
+	 * Overwrites file with decrypted settings
+	 */
+	public static void writeDecryptedSettings()
+	{
+		Properties encryptedProperties = new Properties();
+		try {
+			FileInputStream input = new FileInputStream(file);
+			encryptedProperties.load(input);
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}catch(IOException ie)
+		{
+			ie.printStackTrace();
+		}
+		
+		//replace written settings with decrypted settings
+		encryptedProperties.setProperty("oauth.consumerKey:", 
+				decrypt(encryptedProperties.getProperty("oauth.consumerKey:")));
+		encryptedProperties.setProperty("oauth.consumerSecret:",
+				decrypt(encryptedProperties.getProperty("oauth.consumerSecret:")));
+		encryptedProperties.setProperty("oauth.accessToken:", 
+				decrypt(encryptedProperties.getProperty("oauth.accessToken:")));
+		encryptedProperties.setProperty("oauth.accessTokenSecret:", 
+				decrypt(encryptedProperties.getProperty("oauth.accessTokenSecret:")));
+		
+		try {
+			encryptedProperties.store(output, "Twitter4j Properties");
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 }
